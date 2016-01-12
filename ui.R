fb991027a424372380eaf55474018a8adcab2dd9
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-01-12 15:22:42 +0100

    Modified UI

--- ui.R
+++ ui.R
@@ -2,10 +2,6 @@ library(shiny)
 library(nanocluster)
 
 shinyUI(fluidPage(
-  titlePanel("title panel"),
-  
-  sidebarLayout(
-    sidebarPanel( "sidebar panel"),
-    mainPanel("main panel", nanoClusterOutput("nanocluster"))
-  )
+  titlePanel("Nanocluster"),
+    mainPanel(nanoClusterOutput("nanocluster"))
 ))
