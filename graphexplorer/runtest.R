bfac5c7c38e918c445461c83b00d069d5cf5f116
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-01-13 21:24:59 +0100

    Fixed wrong library import

--- graphexplorer/runtest.R
+++ graphexplorer/runtest.R
@@ -1,5 +1,5 @@
 devtools::install()
-library(nanocluster)
+library(graphexplorer)
 load("~/wkspace.RData")
 graphexplorer(Links = trueLinks, Nodes = trueNodes, Source = "source", Target = "target", Value = "value", NodeID = "name", Group = "group", opacity = 1, zoom = F, bounded = T)
 
