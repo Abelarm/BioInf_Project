2146e1d07818b158b20ea85da18a04eeeae107b8
angy89 <aserra@unisa.it>
2016-01-14 13:06:43 +0100

    interfaccia grafica

--- server.R
+++ server.R
@@ -1,6 +1,41 @@
 library(shiny)
 library(nanocluster)
+setwd("/home/neuronelab/BioInf_Project/")
 
 shinyServer(function(input, output) {
-  output$nanocluster <- renderNanoCluster(nanocluster("/home/marco/BioInf_Project/nano_cluster.json"))
+  DEBUGGING = TRUE
+  
+  UI_query(input,output,nano,drugs,chemical,disease)
+  
+  source("global.R")
+  load("www/nano_based_clustering.RData")
+  load("www/graph_without_genes_also_intra_classes_edges_network_estimation80_2.RData")
+  load("www/entities.RData")
+  load("www/join10.RData")
+  load("www/chemicals_classes.RData")
+  
+  rm(W2_ADJ)
+  
+  observeEvent(input$action1, {
+      res = ADJ_matrix(W_ADJ, input,output,nano,drugs,chemical,disease,chemMat,join10)
+      ADJ2 = res$ADJ2
+      g_clust = res$g_clust
+      
+      
+      data_frame = from_igraph_to_data_frame(g_clust,ADJ2)
+      edges = data_frame$edges
+      vertices = data_frame$vertices
+    
+      output$cluster_output<- renderNanoCluster(
+                  nanocluster(Links = edges, Nodes = vertices,
+                   Source = "source", Target = "target",
+                   Value = "value", NodeID = "name",
+                   Group = "group",zoom = TRUE,opacity = 0.95,fontSize = 10,
+                   legend = TRUE,
+                   charge = -input$repulseration,
+                   linkDistance = JS(paste0("function(d){return d.value*",input$edge_length,
+                                            "}")))
+                   )
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -3,5 +3,29 @@ library(nanocluster)
 
 shinyUI(fluidPage(
   titlePanel("Nanocluster"),
-    mainPanel(nanoClusterOutput("nanocluster"))
+  sidebarPanel(
+    fluidRow(
+        column(3, uiOutput("nano_input")),
+        column(3, uiOutput("drug_input"))
+    ),
+    fluidRow(
+      column(3, uiOutput("disease_input")),
+      column(3, uiOutput("chemical_input"))  
+    ),
+    fluidRow(
+      column(9, sliderInput("repulseration", label = "Repulseration Strenght",
+                            min = 100, max = 10000, value = 4000,step=1))
+    ),
+    fluidRow(
+      column(9, sliderInput("edge_length", label = "Edge length",
+                            min = 2, max = 10, value =2 ,step=1))
+    ),
+    fluidRow(
+      column(4, actionButton("action1", label = "Start", icon=icon("search",lib = "glyphicon")))
+     
+    )
+  ), #end Sidebar
+  mainPanel(
+    wellPanel(nanoClusterOutput("cluster_output"))
+  )
 ))
